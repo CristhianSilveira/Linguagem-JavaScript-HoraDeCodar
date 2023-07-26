f6aa7c9a5ba91a3827cb106a661f54371dee48c3
Cristhian Silveira <129897493+CristhianSilveira@users.noreply.github.com>
2023-07-26 17:53:38 -0300

    DOM

--- Aulas/13- DOM/script.js
+++ Aulas/13- DOM/script.js
@@ -3,7 +3,7 @@
 // 1- Por Tag:
 console.log("Tag:")
 
-var titulo = document.getElementsByTagName('h1')[0];
+var titulo = document.getElementsByTagName('h2')[0];
 console.log(titulo);
 
 var lis = document.getElementsByTagName('li');
@@ -38,9 +38,9 @@ console.log(itensQuery2);
 // ALTERAR CONTEÚDO DO ELEMENTO:
 
 // 1- innerHTML:
-var title = document.querySelector('#title');
+var altera = document.querySelector('#altera');
 
-title.innerHTML = "Testando o texto alterado!";
+altera.innerHTML = "Testando o texto alterado!";
 
 // 2- textContent: -> mais utilizado e recomendado
 var subtitle = document.querySelector('.subtitle');
@@ -53,14 +53,14 @@ subtitle.textContent = "Testando o textContent";
 console.log("Criando elementos:")
 
 // 1- Inserindo no body:
-var body = document.querySelector('body');
+var criar = document.querySelector('#cria');
 var novoParagrafo = document.createElement('p');
 var texto = document.createTextNode("Este é o conteúdo do parágrafo");
 
 novoParagrafo.appendChild(texto);
 console.log(novoParagrafo);
 
-body.appendChild(novoParagrafo);
+criar.appendChild(novoParagrafo);
 
 // 2- Inserindo no container:
 var container = document.getElementById('container');
@@ -77,10 +77,10 @@ container.appendChild(span);
 console.log("Removendo elementos:")
 
 // 1- removendo elemento filho:
-var container = document.querySelector('#container');
-var p = document.querySelector('#container p');
+var exclua = document.querySelector('#exclui');
+var p = document.querySelector('#exclui p');
 
-container.removeChild(p);
+exclua.removeChild(p);
 
 // 2- remover elemento:
 var removido = document.querySelector('#removido');
@@ -90,3 +90,55 @@ removido.remove();
 
 
 // ADICIONANDO ELEMENTO:
+console.log("Adicionando Elementos:")
+
+// 1- appendChild:
+var conteudo = document.querySelector("#conteudo");
+var elemento = document.createElement("div");
+
+elemento.classList = "div-criada";
+conteudo.appendChild(elemento);
+
+// 2- insertBefore:
+var elemento2 = document.querySelector("#conteudo .div-criada");
+var elemento3 = document.createElement("div");
+elemento3.classList = "div-before";
+
+conteudo.insertBefore(elemento3, elemento2);
+
+
+
+// TROCANDO ELEMENTOS:
+console.log("Trocando Elementos:");
+
+var elemento4 = document.createElement("h3");
+elemento4.classList = "testando-classe";
+
+var texto = document.createTextNode("Este é o texto do h3");
+elemento4.appendChild(texto);
+
+var trocar = document.querySelector("#troca");
+var pai = trocar.parentNode;
+pai.replaceChild(elemento4, trocar);
+
+
+
+// ALTERANDO ATRIBUTOS:
+var btn = document.querySelector("#btn");
+btn.setAttribute("disabled", "disabled");
+
+var altera = document.querySelector('.altera');
+altera.setAttribute("class", "alterei-classe");
+
+
+
+// ADICIONANDO CSS PELO DOM:
+
+// 1- Adicionando 1 por 1:
+var comCSS = document.querySelector('#teste-css');
+comCSS.style.color = 'red';
+comCSS.style.backgroundColor = "yellow";
+
+// 2- Adicionando vários estilos:
+comCSS.style.cssText = "color: red; background-Color: yellow; text-decoration: underline;"
+
